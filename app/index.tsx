4cb0a2d7fb6422a65eba807f4ad07da546b76883
Raju Bepary <raju174829@gmail.com>
2025-02-17 20:04:53 +0600

    setup eslint and prettier

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,11 @@
-import { Text, View } from "react-native";
+import { Text, View } from 'react-native';
 
 export default function Index() {
-  return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-blue-600 text-2xl">Hello, EduNexus Family</Text>
-    </View>
-  );
+    return (
+        <View className="flex-1 items-center justify-center">
+            <Text className="text-2xl text-blue-600">
+                Hello, EduNexus Family
+            </Text>
+        </View>
+    );
 }
